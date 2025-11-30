6a4dd6bab92883f9ca0f97145e35e8d8630db8e3
Yusong99 <yusongxu1999@gmail.com>
2025-12-01 00:56:05 +0900

    Initial commit: frontend + backend

---
+++ frontend/app/index.js
@@ -0,0 +1,65 @@
+import {StatusBar} from 'expo-status-bar';
+import {Button, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import {useState} from 'react';
+
+export default function App() {
+    const [userName, setUserName] = useState(1);
+    const [userPwd, setUserPwd] = useState(1);
+    return (
+        <ScrollView>
+            <View style={styles.container}>
+                <TextInput style={styles.input} placeholder={"请输入用户名"} onChangeText={text => setUserName(text)}
+                           defaultValue={userName}>
+                </TextInput>
+                <TextInput style={styles.input} placeholder={"请输入密码"} onChangeText={text => setUserPwd(text)}
+                           defaultValue={userPwd}>
+                </TextInput>
+                <TouchableOpacity style={styles.reload}>
+                    <Text style={styles.label}>登录</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.reload}>
+                    <Text style={styles.label}>注册</Text>
+                </TouchableOpacity>
+            </View>
+        </ScrollView>
+    )
+        ;
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 200,
+        flex: 0.5,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 15,
+        color: 'red',
+        fontWeight: 'bold',
+    },
+    input: {
+        height: 40,
+        width: 300,
+        margin: 12,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: 'red',
+        borderRadius: 5,
+    },
+    reload: {
+        width: 50,
+        marginTop: 10,
+        backgroundColor: '#1f99b0',
+        height: 40,
+        borderRadius: 4,
+        paddingLeft: 10,
+        paddingRight: 10,
+        marginLeft: 100
+    },
+    label: {
+        color: '#fff',
+        lineHeight: 40
+    }
+});
---
+++ frontend/utils/request.js
@@ -0,0 +1,29 @@
+import urlcat from "urlcat";
+const request = async (url, {method = 'GET', params, body} ={}) => {
+    //完整的接口地址
+    const apiUrl = process.env.EXPO_PUBLIC_API_URL;
+    const requestUrl = urlcat(apiUrl, url, params)
+    //请求头
+    const headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    }
+
+    const config = {
+        method,
+        headers,
+        ...(body && { body: JSON.stringify(body) }),
+    }
+
+    const response = await fetch(requestUrl, config)
+
+    if (!response.ok) {
+        const {message, errors} = await response.json().catch(() => null)
+        const error = new Error(message)
+        error.status = response.status;
+        error.errors = errors
+        throw error
+    }
+
+    return await response.json()
+}
